28a37118d10d9ad07e70179833e1f2741aee7186
Sarge322 <shagohod@list.ru>
2023-02-03 23:59:28 +0300

    upd

--- python_blog/python_blog/urls.py
+++ python_blog/python_blog/urls.py
@@ -1,7 +1,7 @@
-"""python_blog URL Configuration
+"""coolsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,38 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+
+from coolsite import settings
+from women.views import *
+from django.urls import path, include
+
 from django.views.static import serve as mediaserve
-
-from blog_theme.views import *
-from  django.urls import path, include
-
-from python_blog import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog_theme.urls')),
-    path('mdeditor/', include('mdeditor.urls')),
     path('captcha/', include('captcha.urls')),
-
+    path('', include('women.urls')),
 ]
+
 if settings.DEBUG:
-    from django.urls import include, path
+    import debug_toolbar
 
     urlpatterns = [
-        path('__debug__/', include('debug_toolbar.urls')),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += [
         url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
             mediaserve, {'document_root': settings.MEDIA_ROOT}),
         url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
-            mediaserve, {'document_root': settings.STATIC_URL}),
+            mediaserve, {'document_root': settings.STATIC_ROOT}),
     ]
-
 
 
 handler404 = pageNotFound
